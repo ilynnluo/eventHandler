183c32dd9017caab934ccabdf7a2551628ef408f
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2024-04-23 22:28:45 -0400

    Add project files.

---
+++ eventHandler/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace eventHandler
+{
+    public delegate void BalanceEventHandler(decimal value);
+    class Bank
+    {
+        private decimal bankBalance;
+        public event BalanceEventHandler balanceChanged;
+
+        public decimal theBalance
+        {
+            set
+            {
+                bankBalance = value;
+                balanceChanged(value);
+            }
+            get
+            {
+                return bankBalance;
+            }
+        }
+    }
+
+    class BalanceLogger
+    {
+        public void balanceLog(decimal amount)
+        {
+            Console.WriteLine("The balance amount is {0}", amount);
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            BalanceLogger logger = new BalanceLogger();
+
+            bank.balanceChanged += logger.balanceLog;
+
+            string deposit;
+            do
+            {
+                Console.WriteLine("How much to deposit?");
+                deposit = Console.ReadLine();
+                if (!deposit.Equals("exit"))
+                {
+                    decimal newValue = decimal.Parse(deposit);
+                    bank.theBalance += newValue;
+                }
+            } while (!deposit.Equals("exit"));
+        }
+    }
+}
